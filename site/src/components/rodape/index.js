90a89c4d15e27222a4c2cb3a39092e4a884b04d9
gomesdantas <biancagomesdantas@gmail.com>
2023-10-04 21:39:16 -0300

    final

---
+++ site/src/components/rodape/index.js
@@ -0,0 +1,78 @@
+
+import './index.scss';
+
+export default function Rodape () {
+  return (
+    <div className="rodape">
+<div className="container">
+            <div className='esquerda2'>
+
+           
+            {/* <img className="logo" src="/assets/images/planta.png" alt="" /> */}
+
+               <h1>ATELIÊ RÉGIA</h1>
+            <p className="paragrafo-esquerda2">O Ateliê Régia é uma empresa </p>
+            <p>desenvolvida e liderada por mulheres com objetivo de peças bonitas, confortáveis e ecológicas.</p>
+<div className="segura">
+            <img className="rece" src="/assets/images/receiclo.png" alt="" />
+
+            <img className="seguro" src="/assets/images/site.png" alt="" />
+            </div>
+            </div>
+
+            <div className="linha"></div>
+           
+        <div className='redes-sociais'>
+
+            <h1>REDES SOCIAIS</h1>
+
+ 
+<div className="icon-redes">
+<img className="i" src="/assets/images/insta.png" alt="" />  
+        <img className="i" src="/assets/images/insta.png" alt="" />
+        <img className="i" src="/assets/images/insta.png" alt="" />
+</div>
+      <div className="redes">
+     
+     <a href="">LOGIN</a>
+     <a href="">CADASTRO</a>
+     <div clas></div>
+     <div className="aaa">
+     <a href="">INSTRUÇÕES</a>
+     <p>Cuidados com as roupas</p>
+     </div>
+     <div> <a href="">Contato:</a>
+        <p>Fale conosco</p>  </div>
+    
+      </div>
+
+         
+
+        </div>
+
+    <div className='suporte'>
+        <h1>SUPORTE</h1>
+<div className="politicas">
+<a>Termos de uso</a>
+    <a>Troca e devoluçõa</a>
+    <a>Política de devoluçõa</a>
+    <a>Politica de privasidade</a>
+    <div className="formas">
+    <h2>FORMAS DE PAGAMENTO</h2>
+    <img className="pagamentos" src="/assets/images/image 288.png" alt="" />
+    </div>
+  
+</div>
+  
+    </div>
+
+      
+
+</div>
+<hr/>
+<div className="details">
+  <p>©2023 ATELIÊ RÉGIA - CNPJ 14341564000100/XXX-XX</p>
+</div>
+         </div>
+  );
+} 
